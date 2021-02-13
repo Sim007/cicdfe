b2104a48042cc6339825af36ef51dcef2793f51e
sim007 <j.sim18011963@gmail.com>
2021-02-13 15:23:25 +0100

    2 extra testen

---
+++ cypress/integration/examples/visitpage.spec.js
@@ -0,0 +1,8 @@
+describe('Visit page', () => {
+  it('Visit a website', () => {
+    cy.visit('www.centric.nl')
+  })  
+//  it('finds the content "type"', () => {  
+//    cy.contains('contains')
+//  })
+})
